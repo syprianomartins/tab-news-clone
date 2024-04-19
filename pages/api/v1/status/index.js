b88f3716c41a596cc04dc022495f6344c815680c
Sypriano <syprianom@gmail.com>
2024-04-18 22:44:53 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
-function status(request, response) {
+import database from "../../../../infra/database";
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 AS sum;");
+  console.log(result.rows);
   response
     .status(200)
     .json({ message: "alunos do curso.dev são acima da media" });
