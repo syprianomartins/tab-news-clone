0c3853612f8cc919aa3ec692fe296897931b8096
Sypriano <syprianom@gmail.com>
2024-07-09 21:21:45 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,33 @@
 import database from "infra/database";
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS sum;");
-  console.log(result.rows);
-  response
-    .status(200)
-    .json({ message: "alunos do curso.dev são acima da media" });
+  const updatedAt = new Date().toISOString();
+
+  const queryVersion = await database.query("SHOW server_version;");
+  const version = queryVersion.rows[0].server_version;
+
+  const queryMaxConnections = await database.query(
+    "SELECT setting AS max_connections FROM pg_settings WHERE name ='max_connections';",
+  );
+  const maxConnections = parseInt(queryMaxConnections.rows[0].max_connections);
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const queryOpenedConnection = await database.query({
+    text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName],
+  });
+  const openedConnections = queryOpenedConnection.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        postgres_version: version,
+        max_connections: maxConnections,
+        opened_connections: openedConnections,
+      },
+    },
+  });
 }
 
 export default status;
